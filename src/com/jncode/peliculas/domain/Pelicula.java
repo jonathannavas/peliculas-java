5d576c8ae4027af8442873a2dfa0e6b99dda9f5a
Jonathan <jonathan.navas9@hotmail.com>
2020-12-05 16:51:53 -0500

    Se agrego la clase de Pelicula

---
+++ src/com/jncode/peliculas/domain/Pelicula.java
@@ -0,0 +1,34 @@
+package com.jncode.peliculas.domain;
+
+/**
+ *
+ * @author Jonathan Navas
+ */
+
+public class Pelicula {
+    
+    private String nombre;
+    
+    public Pelicula(){
+    }
+    
+    public Pelicula(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "nombre=" + nombre + '}';
+    }
+    
+    
+    
+}
